d2c3f22116d6caa85751afe987dccafa43f2da4b
Rahul Kumar <rkgupta30052006@gmail.com>
2026-04-14 00:27:28 +0530

    Add: kth-largest-integer-in-a-stream - submission-1

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-1.cpp
@@ -0,0 +1,25 @@
+class KthLargest {
+private:
+    priority_queue<int, vector<int>, greater<int>> minQ;
+    int cap;
+public:
+    KthLargest(int k, vector<int>& nums) : cap(k) {
+        int n = nums.size();
+        for(int i = 0; i<n; i++){
+            minQ.push(nums[i]);
+            if(minQ.size() > cap){
+                minQ.pop();
+            }
+        }
+    }
+    
+    int add(int val) {
+        if(minQ.size() < cap){
+            minQ.push(val);
+        }else if(minQ.top() < val){
+            minQ.pop();
+            minQ.push(val);
+        }
+        return minQ.top();
+    }
+};
