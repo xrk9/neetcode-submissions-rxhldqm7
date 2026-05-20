8eb07d6dcd1df01b4344286a949c2eb8e8e9a88c
Rahul Kumar <rkgupta30052006@gmail.com>
2026-05-20 18:44:44 +0530

    Add: design-word-search-data-structure - submission-2

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-2.cpp
@@ -0,0 +1,58 @@
+class WordDictionary {
+private:
+    class TrieNode{
+        public:
+            TrieNode* children[26];
+            unordered_set<char> exists;
+            bool isWord;
+            
+            TrieNode(){
+                for(int i = 0; i<26; i++){
+                    children[i] = nullptr;
+                }
+                isWord = false;
+            }
+    };
+    bool helper(int id, const string& word, TrieNode* root) {
+        if(word[id] == '\0'){
+            return root->isWord;
+        }
+        TrieNode* it = root;
+        char c = word[id];
+        if(c == '.'){
+            if(it->exists.empty()) return false;
+            for(const char& ch:it->exists){
+                int idx = ch - 'a';
+                if(helper(id+1, word, it->children[idx])) return true;
+            }
+            return false;
+        }
+        else{
+            int idx = c - 'a';
+            if(!it->children[idx]) return false;
+            return helper(id+1, word, it->children[idx]);
+        }
+    }
+    TrieNode* root;
+public:
+    WordDictionary() {
+        root = new TrieNode();
+    }
+    
+    void addWord(string& word) {
+        TrieNode* it = root;
+        for(const char& c: word){
+            int idx = c - 'a'; // index in children array
+            if(!it->children[idx]){
+                it->children[idx] = new TrieNode();
+                it->exists.insert(c);
+            }
+            it = it->children[idx];
+        }
+        it->isWord = true;
+    }
+    
+    bool search(string& word) {
+        return helper(0, word, root);
+    }
+};
