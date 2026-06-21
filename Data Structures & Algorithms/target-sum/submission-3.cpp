5a36a848e1582252ee5122f58e91b7c72b112cc1
Rahul Kumar <rkgupta30052006@gmail.com>
2026-06-21 23:43:00 +0530

    Add: target-sum - submission-3

---
+++ Data Structures & Algorithms/target-sum/submission-3.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int total_sum = 0;
+        for (int num : nums) {
+            total_sum += num;
+        }
+        if (abs(target) > total_sum || (total_sum + target) & 1 != 0) {
+            return 0;
+        }
+
+        int subset_sum = (total_sum + target) / 2;
+        
+        vector<int> dp(subset_sum + 1, 0);
+        dp[0] = 1;
+
+        for (int num : nums) {
+            for (int i = subset_sum; i >= num; i--) {
+                dp[i] += dp[i - num];
+            }
+        }
+
+        return dp[subset_sum];
+    }
+};
