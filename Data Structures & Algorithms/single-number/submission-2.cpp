50562eb1ee6089c2aaa0e72df5dae52ec53a119c
Rahul Kumar <rkgupta30052006@gmail.com>
2026-08-02 16:29:01 +0530

    Add: single-number - submission-2

---
+++ Data Structures & Algorithms/single-number/submission-2.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int res = 0;
+        for(const auto& num : nums){
+            res^=num;
+        }
+        return res;
+    }
+};
