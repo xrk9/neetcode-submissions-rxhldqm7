a8748da2237a0b6246c4bf19ab9269b2600813a0
Rahul Kumar <rkgupta30052006@gmail.com>
2026-08-06 21:19:49 +0530

    Add: reverse-integer - submission-5

---
+++ Data Structures & Algorithms/reverse-integer/submission-5.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int res = 0;
+        bool neg = x<0;
+        int mod = 0;
+        while(x){
+            mod = x%10;
+            res = res*10 + mod;
+            x/=10;
+        }
+        if((neg && res > 0) || (!neg && res<0) || mod != res%10) return 0;
+
+        return res;
+    }
+};
