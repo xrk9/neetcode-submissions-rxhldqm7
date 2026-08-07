32fb2e7cded4fb84b2aae816f85532002489708e
Rahul Kumar <rkgupta30052006@gmail.com>
2026-08-07 17:14:32 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int count[26] = {0};
+        for(const char& c : s){
+            count[c-'a']++;
+        }
+        for(const char& c : t){
+            count[c-'a']--;
+        }
+        for(int i = 0; i<26; i++){
+            if(count[i] != 0) return false;
+        }
+        return true;
+    }
+};
